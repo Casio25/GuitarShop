467e4e4eb953ecfd7ddcca1beabf7ccf92335967
casio25 <mishakolomiets355@gmail.com>
2023-06-12 06:20:29 +0300

    small design fixes

--- guitar-shop/src/components/MoreInfo/MoreInfo.js
+++ guitar-shop/src/components/MoreInfo/MoreInfo.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import ShoppingCartStore from "../../store/ShoppingCartStore";
+import cart_icon from "../../assets/icons/cart_icon.png"
 import "./MoreInfo.css"
 
 const Modal = ({ active, setActive, product }) => {
+    const [commentStatus, setCommentStatus] = useState(false)
 
+    function loadComments() {
+        if (!commentStatus) {
+            setCommentStatus(true);
+        } else {
+            setCommentStatus(false);
+        }
+    }
     function addToShoppingCart(product) {
         ShoppingCartStore.addToShoppingCart(product);
     }
@@ -12,9 +22,29 @@ const Modal = ({ active, setActive, product }) => {
             <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
                 <div className="modal_content" onClick={(e) => e.stopPropagation()}>
                     <h2>Додати товар до кошику</h2>
-                    <h3 className="modal_name">{product.guitarName}</h3>
-                    <img className="modal_image" src={product.photo} alt="photo" />
-                    <button className="modal_buy_button" onClick={() => addToShoppingCart(product)}>Купити</button>
+                    <div className="guitar_info">
+                        <h3 className="modal_name">{product.guitarName}</h3>
+                        <img className="modal_image" src={product.photo} alt="photo" />
+                        <button className="modal_buy_button" onClick={() => addToShoppingCart(product)}>
+                            <img className="cart_icon" src={cart_icon} alt="cart_icon" />
+                            Купити</button>
+                    </div>
+                        <div className="comment_section">
+                            {!commentStatus && (<button className="load_comments" onClick={() => loadComments(product)}> Завантажити Коменарії</button>)
+                            }
+                            {commentStatus && (
+                                <>
+                                    <div className="all_comments">
+                                        {product.comments.map((comment, index) => (
+                                            <p className="comment" key={index}>{comment}</p>
+                                        ))}
+                                    </div>
+                                    <button className="close_comments" onClick={() => loadComments()}>
+                                        Закрити коментарі
+                                    </button>
+                                </>
+                            )}
+                    </div>
                 </div>
             </div>
         </>
--- guitar-shop/src/components/ShoppingCart/ShoppingCart.js
+++ guitar-shop/src/components/ShoppingCart/ShoppingCart.js
@@ -12,14 +12,14 @@ import "./ShoppingCart.css"
                     className={active ? "modal active" : "modal"}
                     onClick={() => setActive(false)}
                 >
+                    <h2>Кошик</h2>
                     <div className="modal_content" onClick={(e) => e.stopPropagation()}>
-                        <h2>Кошик</h2>
                         <div className="store">
                             {ShoppingCartStore.ShoppingCart.map((product) =>(
                                 <div className="cart_element">
                                     <img className="cart_product_image" src={product.photo} alt="product_image" /> 
                                     <p>{product.guitarName} </p>
-                                    <button onClick={() => ShoppingCartStore.removeFromShoppingCart(product)}>Видалити</button>
+                                    <button className="remove_button" onClick={() => ShoppingCartStore.removeFromShoppingCart(product)}>Видалити</button>
                                 </div>
                             ))}
                         </div>
